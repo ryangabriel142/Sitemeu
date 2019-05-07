9de18d03ff5ef210048511bff3ed3e9e1feac956
ryangabriel142 <48722498+ryangabriel142@users.noreply.github.com>
2019-05-07 11:51:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+	include("cabecalho.php");
+	?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Sobre mim</h2>
+      <div class="fakeimg"><img src="minhafoto.jpg" width="170px" height="200px"></div>
+      <p>Presidente do parlamento jovem da cidade de Sarandi</p>
+      <h3>Alguns dos trabalhos</h3>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Colégio Lisboa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Martiflex</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Parlamento</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Byte cursos</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Sobre o parlamento</h2>
+      <div class="fakeimg"><img src="camara_jovem_sarandi_2018.jpg" width="500px" height="200px"></div>
+      <p>Foto por Ana Zaneti</p>
+
+      <p>Segundo o TRE-PR: "O Parlamento Jovem no Paraná é um projeto de iniciativa do Tribunal Regional Eleitoral do Paraná destinado aos estudantes do Ensino Fundamental e Médio, que pretende trazer para dentro das escolas  o acompanhamento do transcorrer do processo eleitoral de escolha de um candidato a cargo eletivo, oportunizando aos estudantes conhecerem melhor a divisão dos poderes do Estado, em especial o Legislativo, debater problemas da comunidade e deliberar sobre sugestões de possíveis soluções com o intuito de promover maior formação política aos alunos da rede de ensino</p>
+    </div>
+  </div>
+</div>
+
+<?php
+	include("rodape.php");
+	?>
---
+++ quemsomos.php
@@ -0,0 +1,13 @@
+<?php
+	include("cabecalho.php");
+	?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+  
+ 
+
+
+<?php
+	include("rodape.php");
+	?>
